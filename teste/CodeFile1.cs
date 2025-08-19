46729120b5db7a3eda1486a1857c287886e58d94
rajssq <rajssqdev@gmail.com>
2025-08-19 17:35:46 -0400

    Add .gitignore and code files

---
+++ teste/CodeFile1.cs
@@ -0,0 +1,25 @@
+﻿int counter = 10;
+while (counter < 10) //verifica priemiro 
+{
+   Console.WriteLine($"While: {counter}");
+    counter++;
+}
+
+counter = 10;
+do // executa primeiro e 
+{
+    Console.WriteLine($"While: {counter}");
+    counter++;
+} while (counter < 10); // verifica depois
+
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine($"For: {i}");
+}
+
+string[] names = new string[3] { "Ana", "Bia", "Carlos" };
+
+foreach (string name in names)
+{
+    Console.WriteLine($"ForEach: {name}");
+}   
